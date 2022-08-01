0d8d0aa92f5f4d6734da9def48a0efd1c2993a79
mfa34 <105373205+mfa34@users.noreply.github.com>
2022-08-01 14:02:01 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sinir Sayinizi Giriniz :");
+        n= input.nextInt();
+        for (int i =1 ; i<=n ; i*=4){
+            System.out.println(i);
+        }
+        for (int i =1 ; i<=n ; i*=5){
+            System.out.println( i);
+        }
+    }
+}
